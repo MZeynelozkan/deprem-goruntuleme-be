4ef4c63e8cc4d69dc8ed19eb14c10c8f93a1ad2c
Mustafa Eren BAŞOL <merenbasol@gmail.com>
2024-11-04 16:19:00 +0300

    aganiga2

--- models/cityModel.js
+++ models/cityModel.js
@@ -6,6 +6,14 @@ const EarthquakeSchema = new mongoose.Schema({
   depth: { type: Number },
 });
 
+const CitySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+  },
+  recentEarthquakes: [EarthquakeSchema], // Depremleri şehirde listele
+});
 
 const City = mongoose.model("City", CitySchema);
 module.exports = City;
